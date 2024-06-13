789f8e7735717e825229f3a41af5f719c6d29246
Sérgio Altenhofen <sergioalten@gmail.com>
2024-06-13 19:25:12 -0300

    Ajustes script validacao formulario

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,31 @@
-function validarDados(){
-
-    const userName = user.value;
-    const userEmail = email.value;
-    const userAssunto = title.value;
-
-    if(typeof(userName) === 'string'){
-        validacao = 1;
-        alert(validacao)
-    }
-}
 
 const form = document.getElementById('form');
 const user = document.getElementById('user');
 const email = document.getElementById('email');
 const title = document.getElementById('title');
-        
-const validacao = 0;
-        
-if(validacao === 1){
-    const openModalButton = document.querySelector("#btn-form");
-    const closeModalButton = document.querySelector("#btn-modal");
-    const modal = document.querySelector("#modal");
-    const fade = document.querySelector("#fade");
-    
-    const toggleModal = () => {
-        [modal, fade].forEach((el) => el.classList.toggle("hide"));
-    }
-    
-    [openModalButton, closeModalButton, fade].forEach((el) => {
-        el.addEventListener("click", () => toggleModal());
-    })
-} 
+
+
+
+function validarDados(){
+    const validacao = 0;
+    const userValue = user.value.trim();
+   
+}
+
+const openModalButton = document.querySelector("#btn-form");
+const closeModalButton = document.querySelector("#btn-modal");
+const modal = document.querySelector("#modal");
+const fade = document.querySelector("#fade");
+
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+})
+
+const toggleModal = () => {
+    [modal, fade].forEach((el) => el.classList.toggle("hide"));
+}
+         
+[openModalButton, closeModalButton, fade].forEach((el) => {
+    el.addEventListener("click", () => toggleModal());
+})
