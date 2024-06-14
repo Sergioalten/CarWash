fbf0f4bbef5688fb4ec2adb7735a3045d2a6506f
Sérgio Altenhofen <sergioalten@gmail.com>
2024-06-14 09:19:32 -0300

    Finished page contato / Finished form validation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,31 @@
-
-const form = document.getElementById('form');
 const user = document.getElementById('user');
 const email = document.getElementById('email');
 const title = document.getElementById('title');
 
-
-
 function validarDados(){
-    const validacao = 0;
-    const userValue = user.value.trim();
-   
+    let validacao = 0;
+    let userValue = user.value;
+    let userEmail = email.value;
+    let userTitle = title.value;
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+    
+    if(userValue !== ''){
+        if((userEmail !== '') && (emailRegex.test(userEmail))){
+            if(userTitle !== ''){
+                validacao = 1;
+            }
+        } 
+    }
+
+    if (validacao === 1){
+        toggleModal();
+    }
 }
 
 const openModalButton = document.querySelector("#btn-form");
 const closeModalButton = document.querySelector("#btn-modal");
 const modal = document.querySelector("#modal");
 const fade = document.querySelector("#fade");
-
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -26,6 +35,6 @@ const toggleModal = () => {
     [modal, fade].forEach((el) => el.classList.toggle("hide"));
 }
          
-[openModalButton, closeModalButton, fade].forEach((el) => {
+[closeModalButton, fade].forEach((el) => {
     el.addEventListener("click", () => toggleModal());
 })
